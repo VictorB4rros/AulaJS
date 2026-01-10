f8a7e10feee4ca296172b353787b2920d27624a4
Victor Rodrigues <victor.rodrigues@itbeta2.com.br>
2026-01-10 01:14:23 -0300

    dataset

---
+++ dataset/script.js
@@ -0,0 +1,8 @@
+// dataset do elemento html é um objeto que vai ter todos os atributos definidos
+// com o nome de data- no html
+
+const card1 = document.querySelector('[class="card"]');
+const card2 = document.querySelector('[data-product-id="32"]');
+
+console.log(card2);
+
